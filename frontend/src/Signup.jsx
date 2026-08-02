9a191d1b4648c33cd753cb55dcb612370027a398
Sakshi Bute <sakshibute44@gmail.com>
2026-08-02 20:14:07 +0530

    Update Signup.jsx

--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -23,7 +23,7 @@ const handleSignup = async (e) => {
   try {
 
 const response = await fetch(
-  "https://satellite-image-detection.onrender.com/signup",
+  "https://satelliteimagedetection-production.up.railway.app/signup",
   {
     method: "POST",
     headers: {
